b737e936147b25a86a295594c964df8cdcd25b46
Jiri Slaby <jslaby@suse.cz>
2014-07-17 14:01:25 +0200

    some more stable tools
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>

--- perl/StableHelper.pm
+++ perl/StableHelper.pm
@@ -2,6 +2,34 @@ package StableHelper;
 
 use strict;
 
+sub get_upstream_commits(@) {
+	my @res = ();
+	my $cont = 0;
+
+	foreach $_ (@_) {
+		if ($cont) {
+			if (/^\s+([0-9a-f]{40})\s*[\])]$/) {
+				push @res, $1;
+				$cont = 0;
+				next;
+			}
+		}
+		if (/^commit ([0-9a-f]{40}) upstream\.?$/ ||
+			/^[\[(]\s*[Uu]pstream commit ([0-9a-f]{40})\s*[\])]$/ ||
+			/^[uU]pstream commit ([0-9a-f]{40})\.$/ ||
+			/^This is a backport of ([0-9a-f]{40})$/ ||
+			/^\(cherry picked from commit ([0-9a-f]{40})\)$/) {
+			push @res, $1;
+		} elsif (/^[\[(]\s*[Uu]pstream commits ([0-9a-f]{40})\s+and\s*$/) {
+			push @res, $1;
+			$cont = 1;
+		} elsif (/\b[0-9a-f]{40}\b/) {
+#			print "\tUnmatched SHA: $_";
+		}
+	}
+	return @res;
+}
+
 sub add_header($$) {
 	my ($file, $msg) = @_;
 	open(FILE, "<", $file) || die "cannot open $file";
