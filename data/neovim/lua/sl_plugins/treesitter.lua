561db2e50920d209cbcabddb4d05348250848b5a
Jiri Slaby <jirislaby@gmail.com>
2026-04-09 11:01:01 +0200

    nvim: treesitter: set up languages

--- data/neovim/lua/sl_plugins/treesitter.lua
+++ data/neovim/lua/sl_plugins/treesitter.lua
@@ -3,11 +3,48 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-	    require("nvim-treesitter").setup()
+	    local ts = require("nvim-treesitter")
+	    ts.setup()
 	    vim.api.nvim_create_autocmd("FileType", {
-		    callback = function()
-			    pcall(vim.treesitter.start)
-		    end,
+		    callback = function() vim.treesitter.start() end,
+	    })
+	    ts.install({
+		"asm",
+		"bash",
+		"cpp",
+		"c",
+		"cmake",
+		"css",
+		"diff",
+		"dockerfile",
+		"dtd",
+		"git_config",
+		"git_rebase",
+		"gitattributes",
+		"gitcommit",
+		"gitignore",
+		"html",
+		"json",
+		"kconfig",
+		"linkerscript",
+		"llvm",
+		"lua",
+		"make",
+		"markdown",
+		"meson",
+		"ninja",
+		"objdump",
+		"passwd",
+		"perl",
+		"python",
+		"regex",
+		"sql",
+		"strace",
+		"udev",
+		"vim",
+		"vimdoc",
+		"xml",
+		"yaml",
 	    })
     end,
   },
