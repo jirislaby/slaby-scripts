42d2c87680ce6d1db9d723d8125f4c181d6b9e29
Jiri Slaby <jirislaby@gmail.com>
2026-05-21 07:26:22 +0200

    skeleton: update to something more modern

--- data/skeleton/skeleton.cpp
+++ data/skeleton/skeleton.cpp
@@ -1,8 +1,28 @@
-#include <iostream>
-#include <string>
+#include <sl/helpers/Color.h>
+#include <sl/helpers/Exception.h>
+
+using Clr = SlHelpers::Color;
+using RunEx = SlHelpers::RuntimeException;
+//using SlHelpers::raise;
+
+namespace {
+
+void handledMain(int /*argc*/, char **/*argv*/)
+{
+
+}
+
+}
 
 int main(int argc, char **argv)
 {
+	try {
+		handledMain(argc, argv);
+	} catch (const std::exception &e) {
+		Clr(std::cerr, Clr::RED) << e.what();
+		return EXIT_FAILURE;
+	}
 
 	return 0;
 }
+
